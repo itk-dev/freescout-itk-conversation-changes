c254cc08f7c109c6939305b35451ae235fff4891
Martin Yde Granath <yde001@gmail.com>
2025-03-27 09:44:09 +0100

    Update disableStatusChanges.js

--- Public/js/disableStatusChanges.js
+++ Public/js/disableStatusChanges.js
@@ -1,6 +1,8 @@
 let convStatusGroup = document.querySelector("[data-mailbox_id='1'] .conv-info #conv-status.btn-group");
-convStatusGroup.childNodes.forEach(function(item){
-  if ('button' === item.type) {
-    item.setAttribute('disabled', 'disabled')
-  }
-});
+if (convStatusGroup) {
+  convStatusGroup.childNodes.forEach(function(item){
+    if ('button' === item.type) {
+      item.setAttribute('disabled', 'disabled')
+    }
+  });
+}
